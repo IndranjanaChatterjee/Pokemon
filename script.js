d709507a1f132fd6bc3cd0fe070f33e9adafab1a
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2023-07-16 10:47:04 +0530

    changes

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-btn.addEventListener('click',()=>
-{
-    document.getElementById("poke").src="p2.png";
-    document.getElementById("link").style.visibility="visible";
-    
-})
+let audio;
+btn.addEventListener("click", () => {
+  if (btn.innerText == "GO! POKEBALL") {
+    audio = new Audio("ball.mp3");
+    audio.play();
+    document.getElementById("poke").src = "p2.png";
+    document.getElementById("link").style.visibility = "visible";
+    btn.innerText = "Close!Pokeball";
+  } else {
+    document.getElementById("poke").src = "p1.png";
+    document.getElementById("link").style.visibility = "hidden";
+    btn.innerText = "GO! POKEBALL";
+  }
+});
